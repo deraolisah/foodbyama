04abec35b24af151f4f1fae63f47f5d2f959fb48
deraolisah <deraolisah@gmail.com>
2026-03-12 21:08:04 +0100

    Updated: ToNav - Email and Phone

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -5,8 +5,8 @@ const TopNav = () => {
   return (
     <div className="container hidden md:flex items-center justify-between gap-4 h-10 border-b border-gray-200 text-xs">
       <div className="flex items-center gap-3">
-        <p> 09031421223 </p>
-        <p> bespokecollectibles1@gmail.com </p>
+        <a href='tel:+2349054414660'> 09054414660 </a>
+        <a href='mailto:foodbyama4@gmail.com'> foodbyama4@gmail.com </a>
       </div>
 
       <div className="flex items-center gap-3">
